981ac73ef48958de6308c292605710cabec88a32
Gc20040702 <limag656@gmail.com>
2025-02-07 03:47:47 -0300

    Fundamentos de DDD - Getters & Setters das entidades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -11,8 +11,37 @@ interface AnswerProps {
 }
 
 export class Answer extends Entity<AnswerProps> {
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get questionId() {
+    return this.props.questionId;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
   get content() {
     return this.props.content;
+  }
+
+  set cotent(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat("...");
   }
 
   static create(
