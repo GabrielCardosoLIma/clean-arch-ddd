ba18b8e03d530833c5c7c7eff4d879373ef7b817
Gc20040702 <limag656@gmail.com>
2025-02-16 12:28:36 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Deletar comentário da resposta

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -4,7 +4,27 @@ import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment
 export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepository {
   public items: AnswerComment[] = [];
 
+  async findById(id: string) {
+    const answerComment = this.items.find(
+      (item) => item.id.toString() === id
+    );
+
+    if (!answerComment) {
+      return null;
+    }
+
+    return answerComment;
+  }
+
   async create(answerComment: AnswerComment) {
     this.items.push(answerComment);
+  }
+
+  async delete(answerComment: AnswerComment) {
+    const answerCommentIndex = this.items.findIndex(
+      (item) => item.id === answerComment.id
+    );
+
+    this.items.splice(answerCommentIndex, 1);
   }
 }
