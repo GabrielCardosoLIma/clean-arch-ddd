0d21f95648ba32aa13b5f10c5c79064c632b909c
Gc20040702 <limag656@gmail.com>
2025-02-16 12:47:39 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Listar comentários da resposta

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,19 +1,28 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
 import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
 
-export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepository {
+export class InMemoryAnswerCommentsRepository
+  implements AnswerCommentsRepository
+{
   public items: AnswerComment[] = [];
 
   async findById(id: string) {
-    const answerComment = this.items.find(
-      (item) => item.id.toString() === id
-    );
+    const answerComment = this.items.find((item) => item.id.toString() === id);
 
     if (!answerComment) {
       return null;
     }
 
     return answerComment;
+  }
+
+  async findManyByAnswerId(AnswerId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter((item) => item.answerId.toString() === AnswerId)
+      .slice((page - 1) * 20, page * 20);
+
+    return answers;
   }
 
   async create(answerComment: AnswerComment) {
