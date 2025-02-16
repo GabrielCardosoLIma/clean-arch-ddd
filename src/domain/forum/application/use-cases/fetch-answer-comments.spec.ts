f83b74e4ec98a67e194b1a59e7a4416bb8b2f962
Gc20040702 <limag656@gmail.com>
2025-02-16 20:03:24 -0300

    Functional Error Handling - Refatorando testes unitários

--- src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
+++ src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
@@ -8,8 +8,7 @@ let sut: FetchAnswerCommentsUseCase;
 
 describe("Fetch Answer Comments", () => {
   beforeEach(() => {
-    inMemoryAnswerCommentsRepository =
-      new InMemoryAnswerCommentsRepository();
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository();
     sut = new FetchAnswerCommentsUseCase(inMemoryAnswerCommentsRepository);
   });
 
@@ -30,12 +29,12 @@ describe("Fetch Answer Comments", () => {
       })
     );
 
-    const { answerComments } = await sut.execute({
+    const result = await sut.execute({
       answerId: "answer-1",
       page: 1,
     });
 
-    expect(answerComments).toHaveLength(3);
+    expect(result.value?.answerComments).toHaveLength(3);
   });
 
   it("should be able to fetch paginated answer comments", async () => {
@@ -47,11 +46,11 @@ describe("Fetch Answer Comments", () => {
       );
     }
 
-    const { answerComments } = await sut.execute({
+    const result = await sut.execute({
       answerId: "answer-1",
       page: 2,
     });
 
-    expect(answerComments).toHaveLength(2);
+    expect(result.value?.answerComments).toHaveLength(2);
   });
 });
