61efc8cee281316919537f10efb98126bfeff3d3
Gc20040702 <limag656@gmail.com>
2025-02-16 12:21:48 -0300

    Fórum com DDD & Clean Architecture - Caso de uso: Deletar comentário da pergunta

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,10 +1,32 @@
 import { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository";
 import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment";
 
-export class InMemoryQuestionCommentsRepository implements QuestionCommentsRepository {
+export class InMemoryQuestionCommentsRepository
+  implements QuestionCommentsRepository
+{
   public items: QuestionComment[] = [];
+
+  async findById(id: string) {
+    const questionComment = this.items.find(
+      (item) => item.id.toString() === id
+    );
+
+    if (!questionComment) {
+      return null;
+    }
+
+    return questionComment;
+  }
 
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment);
+  }
+
+  async delete(questionComment: QuestionComment) {
+    const questionCommentIndex = this.items.findIndex(
+      (item) => item.id === questionComment.id
+    );
+
+    this.items.splice(questionCommentIndex, 1);
   }
 }
