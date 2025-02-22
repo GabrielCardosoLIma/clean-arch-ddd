f62b7f5a198a3d3bd19a2ecfd5c52b2afd36991b
Gc20040702 <limag656@gmail.com>
2025-02-22 20:05:48 -0300

    Aggregates & Watched Lists - Editando pergunta com anexos

--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -3,6 +3,7 @@ import { QuestionsRepository } from "../repositories/questions-repository";
 import { Question } from "../../enterprise/entities/question";
 import { Either, right } from "@/core/either";
 import { QuestionAttachment } from "../../enterprise/entities/question-attachment";
+import { QuestionAttachmentList } from "../../enterprise/entities/question-attachment-list";
 
 interface CreateQuestionUseCaseRequest {
   authorId: string;
@@ -40,7 +41,7 @@ export class CreateQuestionUseCase {
       });
     });
 
-    question.attachments = questionAttchments;
+    question.attachments = new QuestionAttachmentList(questionAttchments);
 
     await this.questionsRepository.create(question);
 
