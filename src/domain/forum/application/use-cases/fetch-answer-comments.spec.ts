4aa60d6deb30af10e9ede4f7b738ab3e4cd11439
Gc20040702 <limag656@gmail.com>
2025-02-22 20:28:21 -0300

    Aggregates & Watched Lists - Teste de edição da pergunta

--- src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
+++ src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
@@ -1,7 +1,7 @@
 import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-answer-comments-repository";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { FetchAnswerCommentsUseCase } from "./fetch-answer-comments";
-import { makeAnswerComment } from "test/factories/make-comment-answer";
+import { makeAnswerComment } from "test/factories/make-answer-comment";
 
 let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository;
 let sut: FetchAnswerCommentsUseCase;
